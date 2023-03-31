170187cc25174cb4347df0d5f64dc55d6f0781c7
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2023-03-31 20:01:17 +0200

    Remove redundant export

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -3,8 +3,6 @@ import bcrypt from "bcryptjs";
 
 import { prisma } from "~/db.server";
 
-export type { User } from "@prisma/client";
-
 export async function getUserById(id: User["id"]) {
   return prisma.user.findUnique({ where: { id } });
 }
