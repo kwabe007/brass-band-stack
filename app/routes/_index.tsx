94cd86243c61f9b30a02c34148e8a65ed461f068
Kwabena Asante-Poku <kwabena.a92@gmail.com>
2023-04-01 14:49:09 +0200

    Add logout button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 
 import { useOptionalUser } from "~/utils";
 
@@ -31,7 +31,16 @@ export default function Index() {
                 project deployed.
               </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {!user && (
+                {user ? (
+                  <Form action="/logout" method="post">
+                    <button
+                      type="submit"
+                      className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                    >
+                      Logout
+                    </button>
+                  </Form>
+                ) : (
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
                       to="/join"
@@ -65,53 +74,53 @@ export default function Index() {
               {
                 src: "https://upload.wikimedia.org/wikipedia/commons/9/97/Sqlite-square-icon.svg",
                 alt: "SQLite",
-                href: "https://www.sqlite.org/",
+                href: "https://www.sqlite.org/"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157764484-ad64a21a-d7fb-47e3-8669-ec046da20c1f.svg",
                 alt: "Prisma",
-                href: "https://prisma.io",
+                href: "https://prisma.io"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
                 alt: "Tailwind",
-                href: "https://tailwindcss.com",
+                href: "https://tailwindcss.com"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg",
                 alt: "Cypress",
-                href: "https://www.cypress.io",
+                href: "https://www.cypress.io"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157772386-75444196-0604-4340-af28-53b236faa182.svg",
                 alt: "MSW",
-                href: "https://mswjs.io",
+                href: "https://mswjs.io"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157772447-00fccdce-9d12-46a3-8bb4-fac612cdc949.svg",
                 alt: "Vitest",
-                href: "https://vitest.dev",
+                href: "https://vitest.dev"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157772662-92b0dd3a-453f-4d18-b8be-9fa6efde52cf.png",
                 alt: "Testing Library",
-                href: "https://testing-library.com",
+                href: "https://testing-library.com"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
                 alt: "Prettier",
-                href: "https://prettier.io",
+                href: "https://prettier.io"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
                 alt: "ESLint",
-                href: "https://eslint.org",
+                href: "https://eslint.org"
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg",
                 alt: "TypeScript",
-                href: "https://typescriptlang.org",
-              },
+                href: "https://typescriptlang.org"
+              }
             ].map((img) => (
               <a
                 key={img.href}
